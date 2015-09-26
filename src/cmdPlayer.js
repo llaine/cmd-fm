46731bc2eaa8efd58d05b9bfea25957a53e81a61
Jérémy Morin <jer.morin@free.fr>
2015-09-26 10:04:48 +0200

    wip(prompt) test prompt with rl

--- src/cmdPlayer.js
+++ src/cmdPlayer.js
@@ -1,25 +1,57 @@
 'use strict';
 
 import Player from 'player';
+import Readline from 'readline';
+import colors from 'colors';
 
-class CmdPlayer extends Player {
+class CmdPlayer {
 
     constructor(track) {
-        console.log(`Create the player with ${track.title}`);
-        super(track.url);
+        console.log(`Create the player with ${track.title}` .blue);
+        this.player = new Player(track.url);
         this.listSongLoad = [];
         this.listSongLoad.push(track);
+        this.rl = Readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
     }
 
     add(track) {
+        console.log(`Add song in the playlist ${track.title}` .green);
         this.listSongLoad.push(track);
-        console.log(`Add song in the playlist ${track.title}`);
-        super.add(track.url);
+        this.player.add(track.url);
     }
 
     next() {
-        super.next();
-        console.log(`Play ${this.listSongLoad[this.history.length].title}`);
+        console.log(this.player);
+        console.log(`Play ${this.listSongLoad[this.player.history.length].title}` .blue);
+        this.player.next();
+    }
+
+    prompt() {
+        var self = this;
+        self.rl.question('@: ', (answer) => {
+            switch (answer) {
+                case 'n':
+                    self.next();
+                    self.rl.close();
+                    break;
+                case 'p':
+                    self.player.pause();
+                    self.rl.close();
+                    break;
+            }
+        });
+    };
+
+
+    play() {
+        var self = this;
+        self.player.play();
+        self.player.on('playing', () => {
+            self.prompt();
+        });
     }
 }
 
